b55fa17f92d1c146399f52b181e364fa26d623af
ea57 <ea57@alu.ua.es>
2023-02-03 10:35:21 +0100

    servidor funciona, lee y manda mensajes

--- Swift/http_server_test/Sources/http_server_test/Handler.swift
+++ Swift/http_server_test/Sources/http_server_test/Handler.swift
@@ -7,35 +7,37 @@ class Handler:ChannelInboundHandler {
     //typealias OutboundOut = HTTPServerResponsePart
     typealias InboundIn = ByteBuffer
     typealias OutboundOut = ByteBuffer
-    private func httpResponseHead(request: HTTPRequestHead, status: HTTPResponseStatus, headers: HTTPHeaders = HTTPHeaders()) -> HTTPResponseHead {
-    var head = HTTPResponseHead(version: request.version, status: status, headers: headers)
-    let connectionHeaders: [String] = head.headers[canonicalForm: "connection"].map { $0.lowercased() }
 
-    if !connectionHeaders.contains("keep-alive") && !connectionHeaders.contains("close") {
-        // the user hasn't pre-set either 'keep-alive' or 'close', so we might need to add headers
-        switch (request.isKeepAlive, request.version.major, request.version.minor) {
-        case (true, 1, 0):
-            // HTTP/1.0 and the request has 'Connection: keep-alive', we should mirror that
-            head.headers.add(name: "Connection", value: "keep-alive")
-        case (false, 1, let n) where n >= 1:
-            // HTTP/1.1 (or treated as such) and the request has 'Connection: close', we should mirror that
-            head.headers.add(name: "Connection", value: "close")
-        default:
-            // we should match the default or are dealing with some HTTP that we don't support, let's leave as is
-            ()
+    private func httpResponseHead(request: HTTPRequestHead, status: HTTPResponseStatus, headers: HTTPHeaders = HTTPHeaders()) -> HTTPResponseHead {
+        var head = HTTPResponseHead(version: request.version, status: status, headers: headers)
+        let connectionHeaders: [String] = head.headers[canonicalForm: "connection"].map { $0.lowercased() }
+
+        if !connectionHeaders.contains("keep-alive") && !connectionHeaders.contains("close") {
+            // the user hasn't pre-set either 'keep-alive' or 'close', so we might need to add headers
+            switch (request.isKeepAlive, request.version.major, request.version.minor) {
+            case (true, 1, 0):
+                // HTTP/1.0 and the request has 'Connection: keep-alive', we should mirror that
+                head.headers.add(name: "Connection", value: "keep-alive")
+            case (false, 1, let n) where n >= 1:
+                // HTTP/1.1 (or treated as such) and the request has 'Connection: close', we should mirror that
+                head.headers.add(name: "Connection", value: "close")
+            default:
+                // we should match the default or are dealing with some HTTP that we don't support, let's leave as is
+                ()
+            }
         }
+        return head
     }
-    return head
-}
+    
     func channelRead(context: ChannelHandlerContext, data: NIOAny) {
         print("I'am the handler")
         let buff = self.unwrapInboundIn(data)
         let str = buff.getString(at: 0, length: buff.readableBytes) ?? "DEFAULT MESSAGE"
         let sourceAddress = context.channel.remoteAddress!
         let sourceIP = sourceAddress.ipAddress
-        print("Received string: \(str) from \(sourceIP ?? "UNKNOWN")")
+        print("Received string: \(str.trimmingCharacters(in: .whitespacesAndNewlines)) from \(sourceIP ?? "UNKNOWN")")
         var outBuff = context.channel.allocator.buffer(capacity: 100)
-        outBuff.writeString("OLA")
+        outBuff.writeString("OLA\n")
         context.writeAndFlush(self.wrapOutboundOut(outBuff),promise: nil)
         /*var buffer = self.unwrapInboundIn(data)
         //var req:HTTPServerRequestPart 
@@ -66,12 +68,6 @@ class Handler:ChannelInboundHandler {
 
     func channelActive(context: ChannelHandlerContext) {
         print("New connection established.")
-        /*
-        var buffer = context.channel.allocator.buffer(capacity: 100)
-        buffer.writeString("Bienvenido a mi servidor HTTP")
-        context.write(self.wrapOutboundOut(.body(.byteBuffer(buffer))), promise: nil)
-        context.writeAndFlush(self.wrapOutboundOut(.end(nil)), promise: nil)
-        */
     }
 
     func channelReadComplete(context: ChannelHandlerContext) {
