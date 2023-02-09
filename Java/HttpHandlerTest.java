11226a252b2e24990a8e1939dfcb9b976d5e7fe7
ea57 <ea57@alu.ua.es>
2023-02-09 10:49:49 +0100

    servidor probado

--- Java/HttpHandlerTest.java
+++ Java/HttpHandlerTest.java
@@ -1,15 +1,26 @@
 import java.io.IOException;
-import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 public class HttpHandlerTest implements HttpHandler{
     @Override
-    public void handle(HttpExchange t) throws IOException {
-        String response = "Hello World";
-        t.sendResponseHeaders(200, response.length());
-        OutputStream os = t.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
+    public void handle(HttpExchange exchange) throws IOException{
+        switch (exchange.getRequestMethod()){
+            case "GET": handleGet(exchange);
+                        break;
+            default:    System.out.println("not get");
+        }
     }
-}
+
+    public void handleGet(HttpExchange exchange) {
+        byte[] message = "Hola\n".getBytes(StandardCharsets.UTF_8);
+        try {
+            exchange.sendResponseHeaders(200, message.length);
+            exchange.getResponseBody().write(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+} 
