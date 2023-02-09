ad06f72af4cf790164922e9e0c81620e6a4c724b
ea57 <ea57@alu.ua.es>
2023-02-09 16:10:32 +0100

    servidor en java devuelve html

--- Java/HttpHandlerTest.java
+++ Java/HttpHandlerTest.java
@@ -1,6 +1,11 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -15,12 +20,30 @@ public class HttpHandlerTest implements HttpHandler{
     }
 
     public void handleGet(HttpExchange exchange) {
-        byte[] message = "Hola\n".getBytes(StandardCharsets.UTF_8);
+        Headers h = exchange.getResponseHeaders();
+        File newFile = new File("index.html");
+       
+        String line;
+        String response = "";
+
         try {
-            exchange.sendResponseHeaders(200, message.length);
-            exchange.getResponseBody().write(message);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(newFile)));
+            while ((line = bufferedReader.readLine()) != null){
+                response += line;
+            }
+            bufferedReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        h.add("Content-Type", "text/html");
+        try {
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
     }
 } 
