12a97b012920a38c5b1d872e54b18bd7bb7e3a59
ea57 <ea57@alu.ua.es>
2023-02-09 12:56:05 +0100

    servidor en http hecho

---
+++ JS/httpserver.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+
+const host = '127.0.0.1'; 
+const port = 8080; 
+
+const requestListener = function (req, res) { 
+    res.setHeader("Content-Type", "text/html"); 
+    res.writeHead(200); 
+    res.end(`<html><body><h2><Strong> Hello from http server </h2></body></html>`);
+   };
+
+
+const server = http.createServer(requestListener); 
+server.listen(port, host, () => { 
+    console.log(`Server is running on http://${host}:${port}`); 
+}); 
