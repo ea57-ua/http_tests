f99e7f43006c1818aee56519304068b250a5cef4
ea57 <ea57@alu.ua.es>
2023-02-10 11:38:15 +0100

    js medidas hechas

--- JS/httpserver.js
+++ JS/httpserver.js
@@ -7,11 +7,15 @@ const port = 8080;
 const requestListener = function (req, res) { 
     res.setHeader("Content-Type", "text/html"); 
     res.writeHead(200); 
-    res.end(`<html><body><h2><Strong> Hello from http server </h2></body></html>`);
+    res.end(`<html><body><h2><Strong> Hello from http server (JS) </h2></body></html>`);
    };
 
 
 const server = http.createServer(requestListener); 
+
+server.on('connection',(socket) => {
+    console.log("New connection !!");
+})
 server.listen(port, host, () => { 
     console.log(`Server is running on http://${host}:${port}`); 
 }); 
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # http_tests
-Pruebas en diferentes lenguajes de programación para crear un servidor http que acepte peticiones. Se realizarán pruebas en Swift, C++, Java, Rust, Python, Javascript y PHP. 
+Pruebas en diferentes lenguajes de programación para crear un servidor http que acepte peticiones. Se realizarán pruebas en Swift, C++, Java, Rust, Python, Javascript y PHP. También se han hecho medidas de tiempo y memoria usada para cada implementación para realizar una comparación 
+posteriormente. 
 
 Autor: Erik Avagyan.
